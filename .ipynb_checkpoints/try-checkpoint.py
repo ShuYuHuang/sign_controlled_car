c69dea9ea429987de8285bfd7784a32f34f8ce2c
ShuYuHuang <b123767195@gmail.com>
2022-07-05 10:52:01 +0800

    remove ipycheckpoint and cache

--- .ipynb_checkpoints/try-checkpoint.py
+++
@@ -1,8 +0,0 @@
-from pynput.keyboard import Key,KeyCode,Listener
-def on_press(key):
-    
-    print(key,KeyCode(8))
-    return True
-listener=Listener(on_press=on_press)
-listener.start()
-listener.join()
